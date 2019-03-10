a4cb43173f76ab7f424e4b274a8736bb792508a1
Joachim Dreimann <joachim@dreimann.net>
2019-03-10 21:35:00 +0000

    Added colour logging to database

--- sensors/colour.py
+++ sensors/colour.py
@@ -1,9 +1,41 @@
 #!/usr/bin/env python
+import sys
+sys.path.insert(0, '../couchdb')
+import socket
+hostname = socket.gethostname()
 
 import time
-
+import webcolors
+from cloudantclient import client
 from envirophat import light, leds
 
+lightdb = client['all-data']
+
+def closest_colour(requested_colour):
+    min_colours = {}
+    for key, name in webcolors.css3_hex_to_names.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_colour[0]) ** 2
+        gd = (g_c - requested_colour[1]) ** 2
+        bd = (b_c - requested_colour[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    return min_colours[min(min_colours.keys())]
+
+def get_colour_name(requested_colour):
+    try:
+        closest_name = actual_name = webcolors.rgb_to_name(requested_colour)
+    except ValueError:
+        closest_name = closest_colour(requested_colour)
+        actual_name = None
+    return actual_name, closest_name
+
+def accentuate_colours(colour, average):
+    if colour > average:
+      return colour + 80
+    elif colour < average:
+      return colour - 20
+    else:
+      return colour
 
 print("""Shows which colours it can see.
 
@@ -13,19 +45,33 @@ Press Ctrl+C to exit.
 try:
     leds.on()
     while True:
-        r, g, b = light.rgb()
+        requested_colour = light.rgb()
+        avg = sum(requested_colour)/3
+        adjusted_colour = (accentuate_colours(requested_colour[0],avg),accentuate_colours(requested_colour[1],avg),accentuate_colours(requested_colour[2],avg))
+        actual_name, closest_name = get_colour_name(adjusted_colour)
 
+        print "Closest colour name:", closest_name, "but this is probably too dark. RGB", requested_colour, "ADJ", adjusted_colour
 
-        if r > g and r > b:
-          colour = "Red"
-        elif g > r and g > b:
-          colour = "Green"
-        elif b > r and b > g:
-          colour = "Blue"
-        else:
-          colour = "a mix of colours"
+        data = {
+          "name": hostname,
+          "sensor": "light",
+          "created_at": time.strftime("%Y/%m/%d %H:%M:%S +0000", time.gmtime()),
+          "light_level": light.light(),
+          "led_on": True,
+          "colour_name": closest_name,
+          "rgb_raw": {
+            "red": requested_colour[0],
+            "green": requested_colour[1],
+            "blue": requested_colour[2]
+          },
+          "rgb_adjusted": {
+            "red": adjusted_colour[0],
+            "green": adjusted_colour[1],
+            "blue": adjusted_colour[2]
+          }
+        }
 
-        print(str(r) + " Red, " + str(g) + " Green, " + str(b) + " Blue" + " => Is this " + colour + "?")
+        lightdb.create_document(data)
 
         time.sleep(1)
 
